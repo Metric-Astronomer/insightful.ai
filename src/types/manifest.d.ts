1ff1ae128a18f473f862e71a9c04d3d3e1eb3ae7
Metric-Astronomer <ayush1696@gmail.com>
2025-03-11 23:50:22 +0530

    Fix TypeScript declaration conflicts

--- src/types/manifest.d.ts
+++
@@ -1,7 +0,0 @@
-import { ManifestV3Export } from '@crxjs/vite-plugin';
-
-// Change the module declaration to match the import path in vite.config.ts
-declare module './src/manifest.json' {
-  const manifest: ManifestV3Export;
-  export default manifest;
-}
